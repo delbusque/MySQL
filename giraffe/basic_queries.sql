fa6b6c57746af14e57aa3449e675e40753d0238e
delbusque <del.busque@gmail.com>
2024-08-30 11:37:45 +0300

    Update basic_queries.sql

--- giraffe/basic_queries.sql
+++ giraffe/basic_queries.sql
@@ -1,15 +1,3 @@
-describe students;
-
-select * from students;
-
-insert INTO students VALUES (2, "Topi", "Singapoor");
-
-alter Table students add major VARCHAR(20);
-
-alter table students add tests DECIMAL(3, 3);
-
-alter Table students drop tests;
-
 CREATE TABLE Persons (
     PersonID int,
     LastName varchar(255),
@@ -21,3 +9,36 @@ CREATE TABLE Persons (
 DESCRIBE persons;
 
 alter table persons drop address;
+
+insert into
+    persons (PersonID, LastName, FirstName)
+values (2, "Pechev", "Ventsislav");
+
+select * from persons;
+
+create table students (
+    student_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    major VARCHAR(30) UNIQUE,
+    PRIMARY KEY (student_id)
+);
+
+alter Table students add university VARCHAR(40) DEFAULT 'undecided';
+
+describe students;
+
+DROP Table students;
+
+insert INTO students VALUES (2, "Ton", "Mio", "PU");
+
+insert INTO students (name, major) VALUES ('Ben', 'Geology');
+
+insert INTO
+    students (name, major, university)
+VALUES ('Ben', 'Python', 'SoftUni');
+
+select * from students;
+
+alter table students add tests DECIMAL(3, 3);
+
+alter Table students drop tests;
