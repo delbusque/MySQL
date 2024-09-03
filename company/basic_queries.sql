e6a8f3a396cada46692b95da0b93be9ccd0cf0e7
delbusque <del.busque@gmail.com>
2024-09-03 11:11:44 +0300

    Add functions and aggregation

--- company/basic_queries.sql
+++ company/basic_queries.sql
@@ -9,3 +9,18 @@ ORDER BY sex, first_name DESC
 LIMIT 5;
 
 SELECT DISTINCT branch_name FROM branch;
+
+SELECT COUNT(super_id)
+FROM employee
+WHERE
+    sex = 'M'
+    AND birth_day > '1970-01-01';
+
+SELECT SUM(salary) FROM employee;
+
+SELECT AVG(salary) FROM employee;
+
+-- aggregation
+SELECT COUNT(sex), sex FROM employee GROUP BY sex;
+
+SELECT SUM(total_sales), emp_id FROM works_with GROUP BY emp_id;
