df98299e9eb5b5c36743dd08763359744ad6e3f0
delbusque <del.busque@gmail.com>
2024-09-05 10:35:03 +0300

    Added conditional trigger

--- giraffe/triggers.sql
+++ giraffe/triggers.sql
@@ -23,3 +23,20 @@ INSERT INTO
     trigger_insert
 VALUES(NEW.City);
 END $$
+
+CREATE TRIGGER university_trigger_ AFTER
+INSERT ON students FOR EACH ROW BEGIN
+
+IF NEW.university = 'PU' THEN
+INSERT INTO trigger_insert VALUES('Go to Plovdiv ...');
+ELSEIF NEW.university = 'SoftUni' THEN
+INSERT INTO trigger_insert VALUES('I`m Hi-Tech ...');
+ELSE
+INSERT INTO trigger_insert VALUES('Other way !');
+
+END IF;
+END $$
+
+INSERT INTO students VALUES (18, 'Benn', 'Don', 'softuni');
+
+DROP TRIGGER university_trigger;
